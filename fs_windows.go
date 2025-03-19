b1a03800e611c1b52794fcb6768d5b04bd9494b2
fiatjaf <fiatjaf@gmail.com>
2025-03-19 15:10:17 -0300

    add fake `fs` command that doesn't work when compiling for windows but at least compiles.

---
+++ fs_windows.go
@@ -0,0 +1,20 @@
+//go:build windows
+
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/urfave/cli/v3"
+)
+
+var fsCmd = &cli.Command{
+	Name:                      "fs",
+	Usage:                     "mount a FUSE filesystem that exposes Nostr events as files.",
+	Description:               `doesn't work on Windows.`,
+	DisableSliceFlagSeparator: true,
+	Action: func(ctx context.Context, c *cli.Command) error {
+		return fmt.Errorf("this doesn't work on Windows.")
+	},
+}
