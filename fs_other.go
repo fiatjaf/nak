a698c59b0bdb1d45df7edc44682cd269dc6473a0
George <zhoreeq@users.noreply.github.com>
2025-07-23 13:59:49 +0000

    fix build on OpenBSD

--- fs_other.go
+++ fs_other.go
@@ -1,4 +1,4 @@
-//go:build windows
+//go:build windows || openbsd
 
 package main
 
@@ -12,9 +12,9 @@ import (
 var fsCmd = &cli.Command{
 	Name:                      "fs",
 	Usage:                     "mount a FUSE filesystem that exposes Nostr events as files.",
-	Description:               `doesn't work on Windows.`,
+	Description:               `doesn't work on Windows and OpenBSD.`,
 	DisableSliceFlagSeparator: true,
 	Action: func(ctx context.Context, c *cli.Command) error {
-		return fmt.Errorf("this doesn't work on Windows.")
+		return fmt.Errorf("this doesn't work on Windows and OpenBSD.")
 	},
 }
